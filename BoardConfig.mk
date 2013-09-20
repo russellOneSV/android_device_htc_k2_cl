5aeb2e5adbd0fb2b93e3ce2d6221b8d0d6017318
Simon Sickle <simonsimons34@gmail.com>
2013-09-19 21:22:03 -0400

    TARGET_CPU_VARIANT is now used

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,8 @@ TARGET_NO_BOOTLOADER := true
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_VARIANT := cortex-a9
+#TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
